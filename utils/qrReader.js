7221c3a1e838bc52f6e5209a75301189d7f2cd22
jyotshna <jyotshna205@gmail.com>
2022-03-01 18:20:53 +0530

    Fix: Fixed file path reading

--- utils/qrReader.js
+++ utils/qrReader.js
@@ -1,22 +1,28 @@
 const Jimp = require("jimp");
+const fs = require("fs");
 const jsQR = require("jsqr");
 
-const qrReader = (imageBuffer) => {
+const qrReader = (filePath) => {
   return new Promise(async (resolve, reject) => {
-    await Jimp.read(await imageBuffer, (err, image) => {
-      if (err) reject(err);
-      const qrCodeImageArray = new Uint8ClampedArray(image.bitmap.data.buffer);
-      const qrCodeResult = jsQR(
-        qrCodeImageArray,
-        image.bitmap.width,
-        image.bitmap.height
-      );
-      if (qrCodeResult) {
-        resolve(qrCodeResult.data);
-      } else {
-        reject(new Error("Invalid QR code."));
+    await Jimp.read(
+      await fs.readFileSync(__dirname + "/../" + filePath),
+      (err, image) => {
+        if (err) reject(err);
+        const qrCodeImageArray = new Uint8ClampedArray(
+          image.bitmap.data.buffer
+        );
+        const qrCodeResult = jsQR(
+          qrCodeImageArray,
+          image.bitmap.width,
+          image.bitmap.height
+        );
+        if (qrCodeResult) {
+          resolve(qrCodeResult.data);
+        } else {
+          reject(new Error("Invalid QR code."));
+        }
       }
-    });
+    );
   });
 };
 
